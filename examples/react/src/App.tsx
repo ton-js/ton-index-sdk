0d0ed69a90ee1b898a75d47e1b7b0f564bfc1790
Slava Fomin <slava@fomin.io>
2022-12-24 00:42:48 +0300

    Added "react" example project
    Signed-off-by: Slava Fomin <slava@fomin.io>

---
+++ examples/react/src/App.tsx
@@ -0,0 +1,68 @@
+
+import React, { useEffect, useState } from 'react';
+
+import {
+  TonIndexClient,
+  FetchHttpClient,
+  getActiveAccountsCountInPeriod,
+
+} from '@ton.js/ton-index-sdk';
+
+import logo from './logo.svg';
+import './App.css';
+
+
+const dayDurationMs = (24 * 60 * 60 * 1_000);
+
+const indexClient = new TonIndexClient({
+  httpClient: new FetchHttpClient(),
+});
+
+
+function App() {
+
+  const [count, setCount] = useState<number>(0);
+
+  /**
+   * This is just a trivial example, use something like
+   * `react-query` in a real application.
+   */
+  useEffect(() => {
+
+    const interval = setInterval(async () => {
+
+      const yesterday = new Date(
+        Date.now() - dayDurationMs
+      );
+
+      const result = await getActiveAccountsCountInPeriod({
+        client: indexClient,
+        params: {
+          startUtime: yesterday,
+        },
+      });
+
+      setCount(result.count);
+
+    }, 1_000);
+
+    // On unmount
+    return () => clearInterval(interval);
+
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          TON transaction in last 24 hours:<br/>
+          <strong>{ count.toLocaleString('en') }</strong>
+        </p>
+      </header>
+    </div>
+  );
+
+}
+
+export default App;
